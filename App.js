f680083640b40e3f9f45a56f117d38cc8581df7f
Nikhil Pandey <157736609+Nikhil-ux-art@users.noreply.github.com>
2024-07-15 23:07:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './app.css'
+import Navbar from './component/Navbar/Navbar'
+
+const App = () => {
+    return (
+        <>
+        <Navbar/>
+        {/* <Home/>
+        <Popular/>
+        <Offer/>
+        <About/>
+        <Blog/>
+        <Footer/> */}
+        </>
+    )
+}
+export default App
+
+
+
+
+
+
+
+
